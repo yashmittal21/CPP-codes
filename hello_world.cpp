eafd2cd21105cb2d3a598b17961b1a243513d344
BurningShadow2001 <69346250+BurningShadow2001@users.noreply.github.com>
2020-10-22 15:36:03 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,23 @@
+ # include <bits/stdc++.h>
+using namespace std;
+
+# define fast               ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+# define test 	            int chill; cin >> chill ; while (chill--)
+# define fori(i,n)          for(int i=0;i<n;i++)
+# define forl(i,n)          for (ll i=0;i<n;i++)
+# define pb                 push_back
+# define X                  first
+# define Y                  second                      
+# define in_ar_int(arr,n)   int arr[n]; fori(i,n) cin >> arr[i];
+# define in_ar_ll(arr,n)    ll arr[n]; fori(i,n) cin >> arr[i];
+# define endl               "\n"
+# define R0                 return 0
+
+typedef long long ll;
+typedef string str;
+
+
+int main(){
+	fast 
+	cout << "HEllO WORLD" << endl;	
+}
